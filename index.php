698862b07d3dd5beb1feb04834939d6853cd1c6a
Angel <Angelmoreau95@gmail.com>
2021-03-01 15:43:01 +0100

    diaporama semi-fini

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>Immotrott</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
+
 <body>
 	<div class="title">
 		<h1>Immotrott by Muriel Framery</h1>
-		
+
 	</div>
-	
-	<div class="img_defil"></div>
+
+	<div class="img_defil">
+		<div class="container"></div>
+	</div>
 
 	<div class="cont_lien">
 		<div class="lien"></div>
@@ -23,4 +27,5 @@
 	<div class="presentation"></div>
 </body>
 <footer>footer</footer>
+
 </html>
