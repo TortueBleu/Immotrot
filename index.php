37c5029b9900001bed748f2c621acc458282db5c
Admin <lacou.alban@gmail.com>
2021-03-01 13:38:31 +0100

    initalisation du site@:wq

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Immotrott</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="title">
+		<h1>Immotrott by Muriel Framery</h1>
+		
+	</div>
+	
+	<div class="img_defil"></div>
+
+	<div class="cont_lien">
+		<div class="lien"></div>
+		<div class="lien"></div>
+		<div class="lien"></div>
+	</div>
+
+	<div class="anim_trot"></div>
+
+	<div class="presentation"></div>
+</body>
+<footer>footer</footer>
+</html>
