31fd0e9976228917a9f614c07d1b9a63b02438a5
TortueBleu <klarir.games@gmail.com>
2021-03-01 15:44:34 +0100

    presentation Antoine

--- index.php
+++ index.php
@@ -3,10 +3,11 @@
 <head>
 	<title>Immotrott</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="prez.css">
 </head>
 <body>
 	<div class="title">
-		<h1>Immotrott by Muriel Framery</h1>
+		<h1>Immotrott by  <a href="#oui">Muriel Framery</a> </h1>
 		
 	</div>
 	
@@ -20,7 +21,30 @@
 
 	<div class="anim_trot"></div>
 
-	<div class="presentation"></div>
+	<div class="presentation" id="oui">
+	<div class="img">
+				<img src="./img/muriel.png" width="350px">
+			</div>
+		<div class="text1">
+			<h2>A propos de moi : </h2>
+			<z>Muriel Framery <br/></z>
+			<p></br>Consultant immobilier bienveillant</br>
+			<br/>Dans l'île saint louis, entre le marais et le quartier latin, depuis bientôt 7 ans après les Antilles et l'Irlande<br/>
+			<br/>Le goût des autres & le sens du service depuis toujours<br/>
+			<br/>Dans l'immobilier parisien, d'abord agent et à mon compte désormais	<br/></p>
+		</div>
+			
+		<div class="text2">
+			<p><h2>A propos de l'entreprise: </h2>
+			<br/>Mes Experiences SANS préferences:<br/>
+			<br/>	Transaction vente<br/>
+			<br/>	Transaction location<br/>
+			<br/>	Chasse immobilière location & vente<br/></p>
+			
+		</div>
+
+	</div>
+
 </body>
 <footer>footer</footer>
 </html>
