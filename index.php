6d05cc98d3d8edd75456bb1d2a00a579357d4076
Admin <lacou.alban@gmail.com>
2021-03-02 09:10:23 +0100

    ajout des 3 liens (services, ags,bien off)

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<title>Immotrott</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
+
 </head>
 <body>
 	<div class="title">
@@ -11,16 +12,42 @@
 	</div>
 	
 	<div class="img_defil"></div>
-
-	<div class="cont_lien">
-		<div class="lien"></div>
-		<div class="lien"></div>
-		<div class="lien"></div>
+	<div>
+		<div class="popup">test</div>
 	</div>
+	
+	<div class="cont_lien">
+		<div class="lien" id="service">
+			
+			<div class="anim2">
+				<ul>Services:
+					<li>Vente</li>
+					<li>Location</li>
+					<li>Gestion locataire</li>
+					<li>Rénovation</li>
+				</ul>
+			</div>
+		</div>
+		
+		<a href="https://ags-immobilier.com/"> 
+			<div class="lien" id="ags">
+				<div class="anim"><p>Cliquer ici</p></div>
+			</div>
+		</a>
+		
+		
+			<div class="lien" id="bien_off">
+				<div class="anim3"><p>Bien off</p></div>
+			</div>
+	
+		
+	</div>
+		
 
 	<div class="anim_trot"></div>
 
 	<div class="presentation"></div>
 </body>
 <footer>footer</footer>
+<script type="text/javascript" src="script.js"></script>
 </html>
---
+++ script.js
@@ -0,0 +1,8 @@
+let divOff = document.querySelector('#bien_off');
+let popup = document.querySelector('.popup');
+divOff.onclick = test
+
+
+function test(){
+	popup.style.display = 'block';
+}
