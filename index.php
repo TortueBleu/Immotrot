b86f66965da1abfff3421f139f1e397d7c3bf6c7
AxelDemorest <axel.demorest@gmail.com>
2021-03-03 10:35:31 +0100

    Footer terminé

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 
 <head>
 	<title>Immotrott</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+
 	<link rel="stylesheet" type="text/css" href="style.css">
 
 	<link rel="stylesheet" type="text/css" href="trotinette.css">
@@ -21,30 +23,17 @@
 		<h1>Immotrott by <a href="#oui"><strong>Muriel Framery</strong> </a></h1>
 
 	</div>
-	
+
 </header>
 
 <body>
-	
 
-<<<<<<< Updated upstream
+
 	<div class="img_defil">
 		<div class="container"></div>
 		<h3>Acheter un bien Immobillier avec <br>
-		Bienveillance et Sérénité </h3>
+			Bienveillance et Sérénité </h3>
 	</div>
-
-
-	
-=======
->>>>>>> Stashed changes
-
-
-
-<<<<<<< HEAD
-	<div class="img_defil"></div>
-
-	<div class="cont_popup">
 
 		<div class="popup">
 			<ul>Bien Off:
@@ -54,126 +43,128 @@
 				<li>salut</li>
 				<li>salut</li>
 			</ul>
-
-=======
-	
-
-	
-	<div class="cont_popup">
-
-		<div class="popup">
-			<div class="flex_pop">
-				<p>Voici mes offre discrete du moment: </p>
-				<ul>Bien Off:
-					<li>salut</li>
-					<li>salut</li>
-					<li>salut</li>
-					<li>salut</li>
-					<li>salut</li>
-				</ul>
-				<p><a href="">Cliquez ici pour fermer la fenetre</a></p>
-			</div>
-			
-			
->>>>>>> 0d2a2d7d32085c69473f1fb745c2650caac3b53c
 		</div>
-	</div>
 
 
 
+		<div class="cont_popup">
 
-	<div class="cont_lien">
-		<div class="lien" id="service">
+			<div class="popup">
+				<div class="flex_pop">
+					<p>Voici mes offre discrete du moment: </p>
+					<ul>Bien Off:
+						<li>salut</li>
+						<li>salut</li>
+						<li>salut</li>
+						<li>salut</li>
+						<li>salut</li>
+					</ul>
+					<p><a href="">Cliquez ici pour fermer la fenetre</a></p>
+				</div>
 
-			<div class="anim2">
-				<ul>Services:
-					<li>Vente</li>
-					<li>Location</li>
-					<li>Gestion locataire</li>
-					<li>Rénovation</li>
-				</ul>
+
 			</div>
 		</div>
 
-		<a href="https://ags-immobilier.com/">
-			<div class="lien" id="ags">
-				<div class="anim">
-					<p>Cliquer ici</p>
+
+
+
+		<div class="cont_lien">
+			<div class="lien" id="service">
+
+				<div class="anim2">
+					<ul>Services:
+						<li>Vente</li>
+						<li>Location</li>
+						<li>Gestion locataire</li>
+						<li>Rénovation</li>
+					</ul>
 				</div>
 			</div>
-		</a>
+
+			<a href="https://ags-immobilier.com/">
+				<div class="lien" id="ags">
+					<div class="anim">
+						<p>Cliquer ici</p>
+					</div>
+				</div>
+			</a>
 
 
-		<div class="lien" id="bien_off">
-			<div class="anim3">
-				<p>Vente a discrétion</p>
+			<div class="lien" id="bien_off">
+				<div class="anim3">
+					<p>Vente a discrétion</p>
+				</div>
 			</div>
+
+
 		</div>
 
 
-	</div>
+		<div class="anim_trot">
 
+			<div class="debut">
+				<img src="img/trotinette.png" style="width: 97px;"></img>
+				<div class="txt">
+					Je me deplace a trotinette oui
+				</div>
 
-	<div class="anim_trot">
+			</div>
 
-		<div class="debut">
-			<img src="img/trotinette.png" style="width: 97px;"></img>	
-			<div class="txt">
-				Je me deplace a trotinette oui
-		</div>
-
-		</div>
-		
-
-	</div>
-
-	<div class="presentation" id="oui">
-		<div class="img">
-			<img src="./img/muriel.png" width="350px">
-		</div>
-		<div class="text1">
-			<h2>A propos de moi : </h2>
-			<z>Muriel Framery <br /></z>
-			<p></br>Consultant immobilier bienveillant</br>
-				<br />Dans l'île saint louis, entre le marais et le quartier latin, depuis bientôt 7 ans après les Antilles et l'Irlande<br />
-				<br />Le goût des autres & le sens du service depuis toujours<br />
-				<br />Dans l'immobilier parisien, d'abord agent et à mon compte désormais<br />
-			</p>
-		</div>
-
-		<div class="text2">
-			<p>
-			<h2>A propos de l'entreprise: </h2>
-			<br />Mes Experiences SANS préferences:<br />
-			<br />Transaction vente<br />
-			<br />Transaction location<br />
-			<br />Chasse immobilière location & vente<br /></p>
 
 		</div>
 
-	</div>
+		<div class="presentation" id="oui">
+			<div class="img">
+				<img src="./img/muriel.png" width="350px">
+			</div>
+			<div class="text1">
+				<h2>A propos de moi : </h2>
+				<z>Muriel Framery <br /></z>
+				<p></br>Consultant immobilier bienveillant</br>
+					<br />Dans l'île saint louis, entre le marais et le quartier latin, depuis bientôt 7 ans après les Antilles et l'Irlande<br />
+					<br />Le goût des autres & le sens du service depuis toujours<br />
+					<br />Dans l'immobilier parisien, d'abord agent et à mon compte désormais<br />
+				</p>
+			</div>
+
+			<div class="text2">
+				<p>
+				<h2>A propos de l'entreprise: </h2>
+				<br />Mes Experiences SANS préferences:<br />
+				<br />Transaction vente<br />
+				<br />Transaction location<br />
+				<br />Chasse immobilière location & vente<br /></p>
+
+			</div>
+
+		</div>
+
+		<footer>
+			<div class="container_footer">
+				<div>
+					
+				</div>
+				<div>
+					<h2>Contact</h2>
+					<ul>
+						<li style="text-transform: uppercase;">Immotrot</li>
+						<li><i class="fa fa-phone"></i> - 07 63 28 29 84</li>
+						<li><i class="fa fa-envelope"></i> - blabla@gmail.com</li>
+						<li><i class="fa fa-map-marker"></i> - 12 rue des pommiers</li>
+					</ul>
+				</div>
+				<div>
+					<h2>Suivez-nous</h2>
+					<ul>
+						<a href="https://www.linkedin.com/in/murielframery2629/" target="_blank"><li style="margin-top:5px"><i class="fa fa-linkedin"></i></li></a>
+					</ul>
+				</div>
+			</div>
+		</footer>
 
 </body>
-<footer>
-	<div class="container_footer">
-		<div>
-			<h2>À Propos</h2>
-			<p>Dans l'île saint louis, entre le marais et le quartier latin, depuis bientôt 7 ans après les Antilles et l'Irlande. Le goût des autres & le sens du service depuis toujours. Dans l'immobilier parisien, d'abord agent et à mon compte désormais</p>
-		</div>
-		<div>
-			<h2>Contact</h2>
-			<ul>
-				<li>1</li>
-			</ul>
-		</div>
-		<div>
-			<h2>Suivez-nous</h2>
-			<ul>
-				<li>1</li>
-			</ul>
-		</div>
-	</div>
-</footer>
+
 
 <script type="text/javascript" src="script.js"></script>
 
